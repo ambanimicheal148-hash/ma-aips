3d667031d989025c14a62cabcb34e411b9b8ad00
Micheal Ambani <ambanimicheal148@gmail.com>
2026-09-08 20:13:27 +0300

    Prepare private K.A.I.S Master AI Android APK build

---
+++ android/master-ai/app/src/main/java/ke/co/kais/masterai/MainActivity.java
@@ -0,0 +1,3 @@
+package ke.co.kais.masterai;
+import android.app.Activity;import android.os.Bundle;import android.webkit.WebView;import android.webkit.WebViewClient;import android.webkit.WebSettings;
+public class MainActivity extends Activity { private static final String WAR_ROOM="https://ma-aips-ai.hatchable.site/ceo.html"; private WebView web; @Override public void onCreate(Bundle state){super.onCreate(state);web=new WebView(this);WebSettings s=web.getSettings();s.setJavaScriptEnabled(true);s.setDomStorageEnabled(true);s.setSupportZoom(false);s.setBuiltInZoomControls(false);web.setWebViewClient(new WebViewClient());web.loadUrl(WAR_ROOM);setContentView(web);} @Override public void onBackPressed(){if(web.canGoBack())web.goBack();else super.onBackPressed();}}
