3835978eb96cd37680dc47f3daec53beeaaadb0d
Micheal Ambani <ambanimicheal148@gmail.com>
2026-09-11 02:29:37 +0300

    Optimize Master AI Android WebView execution and caching

--- android/master-ai/app/src/main/java/ke/co/kais/masterai/MainActivity.java
+++ android/master-ai/app/src/main/java/ke/co/kais/masterai/MainActivity.java
@@ -1,3 +1,36 @@
 package ke.co.kais.masterai;
-import android.app.Activity;import android.os.Bundle;import android.webkit.WebView;import android.webkit.WebViewClient;import android.webkit.WebSettings;
-public class MainActivity extends Activity { private static final String WAR_ROOM="https://ma-aips-ai.hatchable.site/ceo.html"; private WebView web; @Override public void onCreate(Bundle state){super.onCreate(state);web=new WebView(this);WebSettings s=web.getSettings();s.setJavaScriptEnabled(true);s.setDomStorageEnabled(true);s.setSupportZoom(false);s.setBuiltInZoomControls(false);web.setWebViewClient(new WebViewClient());web.loadUrl(WAR_ROOM);setContentView(web);} @Override public void onBackPressed(){if(web.canGoBack())web.goBack();else super.onBackPressed();}}
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
+import android.graphics.Color;
+import android.view.View;
+
+public class MainActivity extends Activity {
+  private static final String WAR_ROOM = "https://ma-aips-ai.hatchable.site/ceo.html";
+  private WebView web;
+
+  @Override public void onCreate(Bundle state) {
+    super.onCreate(state);
+    web = new WebView(this);
+    web.setBackgroundColor(Color.WHITE);
+    web.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+    WebSettings s = web.getSettings();
+    s.setJavaScriptEnabled(true);
+    s.setDomStorageEnabled(true);
+    s.setDatabaseEnabled(true);
+    s.setCacheMode(WebSettings.LOAD_DEFAULT);
+    s.setMediaPlaybackRequiresUserGesture(false);
+    s.setSupportZoom(false);
+    s.setBuiltInZoomControls(false);
+    web.setWebViewClient(new WebViewClient());
+    web.loadUrl(WAR_ROOM);
+    setContentView(web);
+  }
+
+  @Override public void onBackPressed() {
+    if (web.canGoBack()) web.goBack(); else super.onBackPressed();
+  }
+}
