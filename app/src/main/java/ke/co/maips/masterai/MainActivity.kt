2d07def3dd4ca880855ac272ef9b252a9ff79a3f
Micheal Ambani <ambanimicheal148@gmail.com>
2026-09-13 02:00:43 +0300

    Add Master AI Android launcher

---
+++ app/src/main/java/ke/co/maips/masterai/MainActivity.kt
@@ -0,0 +1,22 @@
+package ke.co.maips.masterai
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : Activity() {
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val web = WebView(this)
+        web.settings.javaScriptEnabled = true
+        web.settings.domStorageEnabled = true
+        web.settings.allowFileAccess = false
+        web.settings.allowContentAccess = false
+        web.webViewClient = WebViewClient()
+        web.loadUrl("https://ma-aips-production.up.railway.app/")
+        setContentView(web)
+    }
+}
