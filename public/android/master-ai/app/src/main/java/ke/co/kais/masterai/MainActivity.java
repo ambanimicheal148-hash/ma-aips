6f900ce8abc1a021a1927bf17c9fb64ce5deeae5
Micheal Ambani <ambanimicheal148@gmail.com>
2026-09-13 04:08:54 +0300

    Add MASTER AI CEO War Room launcher

---
+++ public/android/master-ai/app/src/main/java/ke/co/kais/masterai/MainActivity.java
@@ -0,0 +1,36 @@
+package ke.co.kais.masterai;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private static final String WAR_ROOM = "https://ma-aips-ai.hatchable.site/master-ai-war-room.html";
+    private WebView web;
+
+    @Override public void onCreate(Bundle state) {
+        super.onCreate(state);
+        web = new WebView(this);
+        web.setBackgroundColor(Color.WHITE);
+        web.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+        WebSettings s = web.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setDatabaseEnabled(true);
+        s.setCacheMode(WebSettings.LOAD_DEFAULT);
+        s.setMediaPlaybackRequiresUserGesture(false);
+        s.setSupportZoom(false);
+        s.setBuiltInZoomControls(false);
+        web.setWebViewClient(new WebViewClient());
+        web.loadUrl(WAR_ROOM);
+        setContentView(web);
+    }
+
+    @Override public void onBackPressed() {
+        if (web.canGoBack()) web.goBack(); else super.onBackPressed();
+    }
+}
