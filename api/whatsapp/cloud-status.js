aa0023c423d95fa8795c97be329bd05ea67b5a59
Micheal Ambani <ambanimicheal148@gmail.com>
2026-09-20 02:18:52 +0300

    Add WhatsApp Cloud API integration

---
+++ api/whatsapp/cloud-status.js
@@ -0,0 +1,5 @@
+import { getCloudStatus } from "../../lib/whatsapp-cloud.js";
+export default async function handler(req, res) {
+  if (req.method !== "GET") return res.status(405).json({ error: "Method not allowed" });
+  return res.status(200).json(getCloudStatus());
+}
